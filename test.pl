f34cebeda48d427282cef79e9071c2000307b792
Vasiliy Vylegzhanin <v.gadfly@gmail.com>
2019-09-23 19:08:39 +0300

    test.pl: implicit Storage creation

--- test.pl
+++ test.pl
@@ -1,11 +1,12 @@
 use Modern::Perl;
 
 use Teleperl;
+use Teleperl::Storage;
 
 use AnyEvent;
 
 my $cv = AE::cv;
-my $t = Teleperl->new( force_new_session => 1 );
+my $t = Teleperl->new( force_new_session => 1, storage => Teleperl::Storage->new );
 $t->reg_cb( error => sub { $cv->send } );
 $t->start;
 $cv->recv;
